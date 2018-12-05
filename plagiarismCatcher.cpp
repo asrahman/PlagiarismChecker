21f3020c5b70ca23cc6d1dbd1729fcc2ad426feb
Shania Paul <shaniapaul4@gmail.com>
2018-12-05 12:01:11 -0600

    hash table

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -23,7 +23,8 @@ struct FileNode
     FileNode* next;
 };
 
-vector<FileNode*> HashTable[tableSize];
+FileNode* HashTable[tableSize];
+
 
 
 int hashFunction(string chunk){
@@ -55,10 +56,12 @@ int getdir (string dir, vector<string> &files)
 int main(int argc, char* argv[])
 {
     deque<string> myWords; //new deck with no words
-    string dir = string(argv[1]);
+    //string dir = string(argv[1]);
+    string dir = string("sm_doc_set");
     vector<string> files = vector<string>();
 
-    int chunkSize = atoi(argv[2]);
+    //int chunkSize = atoi(argv[2]);
+    int chunkSize = 8;
 
     getdir(dir,files);
 
@@ -71,8 +74,9 @@ int main(int argc, char* argv[])
     cout<<files[2];
     cout<<endl;
     cout<<getdir(dir,files);
-    cout<<argv[1]<<endl;
-    string str = string(argv[1]) + "/" + files[2]; //change to 2
+    //cout<<argv[1]<<endl;
+    //string str = string(argv[1]) + "/" + files[2]; //change to 2
+    string str = string("sm_doc_set/" + files[2]);
     inFile.open(str.c_str());
     if(inFile.is_open()){
         cout << "File Open" << endl;
@@ -104,17 +108,28 @@ int main(int argc, char* argv[])
 
     deque<string>::iterator it = myWords.begin();
 
+    for(int i = 0; i<tableSize; i++){
+        HashTable[i] = NULL;
+    }
+
     while (myWords.size() > chunkSize) {
         string str1 = "";
         for (int i = 0; i < chunkSize; i++) {
-            cout << *it << " ";
+            //cout << *it << " ";
             str1 += *it;
             it++;
         }
         int tableIndex = hashFunction(str1);
-        FileNode* temp;
-        temp = HashTable[tableIndex];
+        if(HashTable[tableIndex] == NULL) {
+            HashTable[tableIndex]->fileIndex = 2;
+            HashTable[tableIndex]->next = NULL;
+        }else{
+            FileNode* temp = HashTable[tableIndex];
+            HashTable[tableIndex]->fileIndex = 2;
+            HashTable[tableIndex]->next = temp;
+        }
         cout << tableIndex << endl;
+        cout << HashTable[tableIndex]->fileIndex;
         myWords.pop_front();
         it = myWords.begin();
         //cout<<endl;
